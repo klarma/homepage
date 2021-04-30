e0d354a74186d4470fe675ff408ea90e56b06722
Katarzyna Napierala <k_napierala@op.pl>
2021-04-30 12:49:42 +0200

    functions added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
-let contactButton = document.querySelector(".js-section__contactButton");
-let contactData = document.querySelector(".js-section__contactData");
-contactButton.addEventListener("click", () => {
-    contactData.classList.toggle("section__contactData--show");
-    contactButton.innerText = contactData.classList.contains("section__contactData--show") ? "Ukryj" : "Kontakt";
-});
+{
+    const welcome = () => {
+        console.log("Hello world created by Krzysiu. Nice to be here");
+    }
 
-console.log("Hello world created by Krzysiu. Nice to be here");
+    const toogleContactData = (contactButton) => {
+        const contactData = document.querySelector(".js-section__contactData");
+        contactData.classList.toggle("section__contactData--show");
+        contactButton.innerText = contactData.classList.contains("section__contactData--show") ? "Ukryj" : "Kontakt";
+    }
+
+    const init = () => {
+        const contactButton = document.querySelector(".js-section__contactButton");
+        contactButton.addEventListener("click", () => {
+            toogleContactData(contactButton)
+        });
+        welcome()
+    }
+
+    init();
+}
