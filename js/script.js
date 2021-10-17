9f91250755dbb76877c63e3f789683d8442a3f3c
Katarzyna Napierala <k_napierala@op.pl>
2021-10-17 18:29:56 +0200

    typo corrected in script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
         console.log("Hello world created by Krzysiu. Nice to be here");
     }
 
-    const toogleContactData = (contactButton) => {
+    const toggleContactData = (contactButton) => {
         const contactData = document.querySelector(".js-section__contactData");
         contactData.classList.toggle("section__contactData--show");
         contactButton.innerText = contactData.classList.contains("section__contactData--show") ? "Ukryj" : "Kontakt";
@@ -12,7 +12,7 @@
     const init = () => {
         const contactButton = document.querySelector(".js-section__contactButton");
         contactButton.addEventListener("click", () => {
-            toogleContactData(contactButton)
+            toggleContactData(contactButton)
         });
         welcome()
     }
